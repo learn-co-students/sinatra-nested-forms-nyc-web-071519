9eb0cd21473b1c79e0bffe84641b70a9f61375bb
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-07 13:33:37 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+    @@all = []
+    def initialize(name, type, booty_attr)
+        @name = name
+        @type = type
+        @booty_attr = booty_attr
+        Ship.all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
